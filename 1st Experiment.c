78af9db8ed0ce308678ffb8ad08e523c3738cf58
maddy13904 <67920211+maddy13904@users.noreply.github.com>
2023-11-18 10:16:00 +0530

    Add files via upload

---
+++ 1st Experiment.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+	int n,i;
+	printf("Name: S. Madhavan\nReg.no: 192210580");
+	printf("\nEnter last number of series:");
+	scanf("%d",&n);
+	for(i=1;i<=n;i++)
+	{
+		printf("%d\n",i);
+	}
+	return 0;
+}
